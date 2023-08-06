ec76d2215d0db7f78619662e4d38931f3c66fd38
Hicks Jerilyn <sylviashu@mail.ru>
2023-08-06 20:54:14 +0700

    sslvvb

--- src/app/main.py
+++ src/app/main.py
@@ -2,8 +2,16 @@ from presenter.presenter import Presenter
 from model.model import Model
 from view.view import View
 
+import customtkinter
+
 if __name__ == "__main__":
+    root = customtkinter.CTk()
+
     model = Model()
-    view = View()
+    view = View(root)
     presenter = Presenter(model, view)
-    presenter.run_app()
+
+    root.mainloop()
+
+
+# https://www.google.com/search?q=mvp+python+example&tbm=vid&source=lnms&sa=X&ved=2ahUKEwjBxIb_kciAAxUESfEDHaFmAawQ0pQJegQICxAB&biw=1503&bih=966&dpr=2#fpstate=ive&vld=cid:2b3e89d3,vid:eHhXoCNCI1c
--- src/app/presenter/presenter.py
+++ src/app/presenter/presenter.py
@@ -3,10 +3,7 @@ class Presenter:
         self.model = model
         self.view = view
 
-    def run_app(self):
-        self.view.mainloop()
-
-    def handle_user_action(self):
-        data = self.view.get_user_input()
-        result = self.model.perform_operation(data)
-        self.view.update_result(result)
+    # def handle_user_action(self):
+    #     data = self.view.get_user_input()
+    #     result = self.model.perform_operation(data)
+    #     self.view.update_result(result)
